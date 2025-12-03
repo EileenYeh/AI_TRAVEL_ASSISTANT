ea5fe204ca8bfa7d108c6099174a9bcec61aaa2d
Lea Hayoun <hayounlea@gmail.com>
2025-12-03 12:00:51 +0100

    adding validates to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,20 @@ class Trip < ApplicationRecord
   belongs_to :user
   has_many :chats, dependent: :destroy
 
+  validates :start_date, :end_date, :destination_country, :departure_city,
+            :travelers_count, :budget, presence: true
+
+  validates :travelers_count, :budget, numericality: { greater_than: 0 }
+
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if start_date.blank? || end_date.blank?
+    if end_date < start_date
+      errors.add(:end_date, "doit être après la date de départ")
+    end
+  end
+
 end
