f281f634abc32305508cb631a0b7cc0f6634ea9d
Lea Hayoun <hayounlea@gmail.com>
2025-12-04 14:55:05 +0100

    added sidebar / sidenav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_trips # dans private
 
   protected
 
@@ -13,4 +14,11 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
+
+private
+
+  def set_trips # permet de définir trips pour toutes les pages, ici je l'ai mis pour que la sidebar soit sur toutes les pages
+    @trips = current_user&.trips
+  end
+
 end
